5f118ee6b62b3aa2c2954dabe1222e821058d699
radius-gagir <radiusgagir@radius-gagirs-MacBook-Air.local>
2025-12-16 15:58:54 +0300

    feat: threshold and reconstruct added

--- wavelet_utils.py
+++ wavelet_utils.py
@@ -31,6 +31,24 @@ def perform_wavelet(gray,wavelet_name):
     c2=pywt.dwt2(gray,wavelet_name)
     return c2
 
+#wavelet uygulanmış matrise tresholdları uygulayan (sıkıştıran metot)
+def apply_threshold(coeff_array,T):
+    copy=coeff_array.copy()
+    smalls=np.abs(copy)<T
+    copy[smalls]=0
+    smalls_count=smalls.sum()
+    return copy,smalls_count
+
+#sıkıştırılmış resmi eski haline dönüştüren metot
+def reconstruct(coeffs2_thr,gray,WAVELET_NAME):
+    rec = pywt.idwt2(coeffs2_thr, WAVELET_NAME)
+    print("reconstructed shape:", rec.shape)
+    min_h = min(gray.shape[0], rec.shape[0])
+    min_w = min(gray.shape[1], rec.shape[1])
+
+    gray_c = gray[:min_h, :min_w]
+    reconstructed_c  = rec[:min_h, :min_w]
+    return gray_c,reconstructed_c
 
 #resmi hazırla
 gray=prep_img(path)
@@ -38,3 +56,13 @@ gray=prep_img(path)
 #wavelet uygula
 coeffs2=perform_wavelet(gray,WAVELET_NAME)
 coeff_A,(coeff_H,coeff_V,coeff_D)=coeffs2
+
+#treshold uygula
+coeff_A_thr, num_of_zeroed_coeffA  = apply_threshold(coeff_A, TRESHOLD)
+coeff_H_thr, num_of_zeroed_coeffH  = apply_threshold(coeff_H, TRESHOLD)
+coeff_V_thr, num_of_zeroed_coeffV  = apply_threshold(coeff_V, TRESHOLD)
+coeff_D_thr, num_of_zeroed_coeffD  = apply_threshold(coeff_D, TRESHOLD)
+
+#resmi geri oluştur
+coeffs2_thr = (coeff_A_thr, (coeff_H_thr, coeff_V_thr, coeff_D_thr))
+gray_c,reconstructed_c=reconstruct(coeffs2_thr,gray,WAVELET_NAME)
